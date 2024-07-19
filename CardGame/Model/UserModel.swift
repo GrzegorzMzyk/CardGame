84d7f6f458d2dc1b5b041cba017d6e6ffe8fd3a2
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-07-19 21:39:12 +0200

    [clean]

--- CardGame/Model/UserModel.swift
+++ CardGame/Model/UserModel.swift
@@ -7,16 +7,6 @@
 
 import Foundation
 
-//struct UserModel: Identifiable {
-//    let id: String = UUID().uuidString
-//    let userName: String
-//    let points: Int
-//    
-//    init(userName: String, points: Int) {
-//        self.userName = userName
-//        self.points = points
-//    }
-//}
 import Observation
 
 @Observable class UserModel: Identifiable {
